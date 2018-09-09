9183c47ab4d4475d22c0af24ba3b87271f1a1ec4
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-10 01:02:03 +0300

    ürünler modülünün ürün listesi için html şablonu oluşturuldu

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,56 @@
-product list content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>Url</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durumu</th>
+                    <th>İşlem</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#1</td>
+                        <td>Monitör-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılabilen monitör askısıdır.</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#1</td>
+                        <td>Monitör-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılabilen monitör askısıdır.</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#1</td>
+                        <td>Monitör-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece kullanılabilen monitör askısıdır.</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" type="button" class="btn btn-danger btn-sm btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" type="button" class="btn btn-info btn-sm btn-outline"><i class="fa fa-pencil"></i> Düzenle</a>
+                        </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
