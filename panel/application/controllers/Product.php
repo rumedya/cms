3a038a1fb6884e2115f28e91fb45d71b69854676
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-16 13:02:28 +0300

    ürün ekle formu için form validation entegre edildi.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -41,7 +41,32 @@ class Product extends CI_CONTROLLER{
 
     }
     public function save(){
-        echo "saved";
+
+        $this->load->library("form_validation");
+
+        //kurallar yazılır...
+        $this->form_validation->set_rules("title","Başlık","required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required"  => "{field} alanı doldurulmalıdır."
+            )
+        );
+
+        //form valitadion çalıştırılır
+
+        //TRUE - FALSE
+        $validate = $this->form_validation->run();
+
+        if($validate){
+            echo "Kayıt işlemleri başlar...";
+        }else{
+            echo validation_errors();
+        }
+        //başarılı ise kayıt işlemi başlar
+            //kayıt işlemi başlar
+        //başarısız ise
+            //hata ekranda gösterilir...
     }
 
 
