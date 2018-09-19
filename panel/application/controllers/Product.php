6cc8993cbd3ab97947c6ebc9419da064c02a593e
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-19 22:37:45 +0300

    ürün update işlemi gerçekleştirildi.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -118,6 +118,73 @@ class Product extends CI_CONTROLLER{
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
 
     }
+    public function update($id){
+
+        $this->load->library("form_validation");
+
+        //kurallar yazılır...
+        $this->form_validation->set_rules("title","Başlık","required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required"  => "{field} alanı doldurulmalıdır."
+            )
+        );
+
+        //form valitadion çalıştırılır
+
+        //TRUE - FALSE
+        $validate = $this->form_validation->run();
+
+        //Monitör Askısı
+        //monitor-askisi
+
+
+        if($validate){
+
+            $update = $this->product_model->update(
+                array(
+                    "id"            =>  $id
+                ),
+                array(
+                    "title"         =>  $this->input->post("title"),
+                    "description"   =>  $this->input->post("description"),
+                    "url"           =>  convertToSEO($this->input->post("title")),
+                )
+            );
+            //TODO ALERT SİSTEMİ EKLENECEK
+            if($update){
+                redirect(base_url("product"));
+            }else{
+                redirect(base_url("product"));
+            }
+
+        }else{
+            $viewData                   = new stdClass();
+
+            /**Tablodan Verilerin Getirilmesi*/
+            $item = $this->product_model->get(
+                array(
+                    "id"        => $id,
+                )
+            );
+
+            /** View'e gönderilecek değişkenlerin set edilmesi */
+            $viewData->viewFolder       = $this->viewFolder;
+            $viewData->subViewFolder    = "update";
+            $viewData->form_error       = true;
+            $viewData->item             = $item;
+
+
+
+
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);        }
+        //başarılı ise kayıt işlemi başlar
+        //kayıt işlemi başlar
+        //başarısız ise
+        //hata ekranda gösterilir...
+    }
+
 
 
 }
--- panel/application/models/Product_model.php
+++ panel/application/models/Product_model.php
@@ -3,6 +3,7 @@
 class Product_model extends CI_Model{
 
     public $tableName = "products";
+
     public function __construct()
     {
         parent::__construct();
@@ -22,8 +23,15 @@ class Product_model extends CI_Model{
     public function get_all($where = array()){
         return $this->db->where($where)->get($this->tableName)->result();
     }
+
     public function add($data = array()){
         return $this->db->insert($this->tableName, $data);
+
+    }
+
+    public function update($where = array(), $data = array()){
+
+        return $this->db->where($where)->update($this->tableName, $data);
 
     }
 
