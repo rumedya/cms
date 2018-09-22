92c669c7be5a4bc7bd76f571df347cef488dc891
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-22 21:31:24 +0300

    upload işlemi yapıldı

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -5,10 +5,10 @@
             <div class="widget-body">
 
 
-                <form action="../api/dropzone" class="dropzone" data-plugin="dropzone" data-options="{ url: '../api/dropzone'}">
+                <form action="<?php echo base_url("product/image_upload");?>" class="dropzone" data-plugin="dropzone" data-options="{ url: '<?php echo base_url("product/image_upload");?>'}">
                     <div class="dz-message">
-                        <h3 class="m-h-lg">Drop files here or click to upload.</h3>
-                        <p class="m-b-lg text-muted">(This is just a demo dropzone. Selected files are not actually uploaded.)</p>
+                        <h3 class="m-h-lg">Yüklemek istediğiniz resimleri buraya sürükleyiniz.</h3>
+                        <p class="m-b-lg text-muted">(Yüklemek için dosyalarınızı sürükleyiniz yada buraya tıklayınız)</p>
                     </div>
                 </form>
             </div><!-- .widget-body -->
@@ -19,7 +19,7 @@
 <div class="row">
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Ürünün fotoğrafları
+            <b><?php echo $item->title; ?></b> kaydına ait resimler.
             <a href="#" type="button" class="btn pull-right btn-primary btn-xs btn-outline"><i class="fa fa-plus"></i> Yeni Ekle</a>
         </h4>
 
