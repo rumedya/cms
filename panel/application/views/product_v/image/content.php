ac907d6fbffa9beab9ee91e27b3ef31c4b4569cc
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-22 21:04:36 +0300

    css ile ürün fotoğraflarının tasarımında değişiklikler yapıldı

--- panel/application/views/product_v/image/content.php
+++ panel/application/views/product_v/image/content.php
@@ -1,12 +1,5 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">
-            Ürünün fotoğrafları
-            <a href="#" type="button" class="btn pull-right btn-primary btn-xs btn-outline"><i class="fa fa-plus"></i> Yeni Ekle</a>
-        </h4>
-
-    </div><!-- END column -->
-    <div class="col-md-12">
         <div class="widget">
 
             <div class="widget-body">
@@ -36,7 +29,7 @@
 
             <div class="widget-body">
 
-                <table class="table table-bordered table-striped table-hover">
+                <table class="table table-bordered table-striped table-hover pictures_list">
                     <thead>
                         <th>#id</th>
                         <th>Görsel</th>
@@ -46,10 +39,10 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td>#1</td>
-                            <td><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100 text-center"><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
                             <td>deneme-urunu.jpg</td>
-                            <td>
+                            <td class="w100 text-center">
                                 <input
                                         data-url="<?php echo base_url("product/isActiveSetter/"); ?>"
                                         class="isActive"
@@ -60,19 +53,19 @@
                                     <?php echo (true) ? "checked" : ""; ?>
                                 />
                             </td>
-                            <td>
+                            <td class="w100 text-center">
                                 <button
                                         data-url="<?php echo base_url("product/delete/"); ?>" type="button"
-                                        class="btn btn-danger btn-xs btn-outline remove-btn">
+                                        class="btn btn-danger btn-xs btn-outline remove-btn btn-block">
                                     <i class="fa fa-trash"></i> Sil
                                 </button>
                             </td>
                         </tr>
                         <tr>
-                            <td>#1</td>
-                            <td><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100 text-center"><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
                             <td>deneme-urunu.jpg</td>
-                            <td>
+                            <td class="w100 text-center">
                                 <input
                                         data-url="<?php echo base_url("product/isActiveSetter/"); ?>"
                                         class="isActive"
@@ -83,19 +76,19 @@
                                     <?php echo (true) ? "checked" : ""; ?>
                                 />
                             </td>
-                            <td>
+                            <td class="w100 text-center">
                                 <button
                                         data-url="<?php echo base_url("product/delete/"); ?>" type="button"
-                                        class="btn btn-danger btn-xs btn-outline remove-btn">
+                                        class="btn btn-danger btn-xs btn-outline remove-btn btn-block">
                                     <i class="fa fa-trash"></i> Sil
                                 </button>
                             </td>
                         </tr>
                         <tr>
-                            <td>#1</td>
-                            <td><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100 text-center"><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
                             <td>deneme-urunu.jpg</td>
-                            <td>
+                            <td class="w100 text-center">
                                 <input
                                         data-url="<?php echo base_url("product/isActiveSetter/"); ?>"
                                         class="isActive"
@@ -106,19 +99,19 @@
                                     <?php echo (true) ? "checked" : ""; ?>
                                 />
                             </td>
-                            <td>
+                            <td class="w100 text-center">
                                 <button
                                         data-url="<?php echo base_url("product/delete/"); ?>" type="button"
-                                        class="btn btn-danger btn-xs btn-outline remove-btn">
+                                        class="btn btn-danger btn-xs btn-outline remove-btn btn-block">
                                     <i class="fa fa-trash"></i> Sil
                                 </button>
                             </td>
                         </tr>
                         <tr>
-                            <td>#1</td>
-                            <td><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100 text-center"><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
                             <td>deneme-urunu.jpg</td>
-                            <td>
+                            <td class="w100 text-center">
                                 <input
                                         data-url="<?php echo base_url("product/isActiveSetter/"); ?>"
                                         class="isActive"
@@ -129,10 +122,33 @@
                                     <?php echo (true) ? "checked" : ""; ?>
                                 />
                             </td>
-                            <td>
+                            <td class="w100 text-center">
                                 <button
                                         data-url="<?php echo base_url("product/delete/"); ?>" type="button"
-                                        class="btn btn-danger btn-xs btn-outline remove-btn">
+                                        class="btn btn-danger btn-xs btn-outline remove-btn btn-block">
+                                    <i class="fa fa-trash"></i> Sil
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="w100 text-center">#1</td>
+                            <td class="w100 text-center"><img width="30" src="<?php echo base_url();?>201.jpg" alt="" class="img-responsive" </td>
+                            <td>deneme-urunu.jpg</td>
+                            <td class="w100 text-center">
+                                <input
+                                        data-url="<?php echo base_url("product/isActiveSetter/"); ?>"
+                                        class="isActive"
+                                        type="checkbox"
+                                        data-switchery
+                                        data-color="#10c469"
+                                        data-size="small"
+                                    <?php echo (true) ? "checked" : ""; ?>
+                                />
+                            </td>
+                            <td class="w100 text-center">
+                                <button
+                                        data-url="<?php echo base_url("product/delete/"); ?>" type="button"
+                                        class="btn btn-danger btn-xs btn-outline remove-btn btn-block">
                                     <i class="fa fa-trash"></i> Sil
                                 </button>
                             </td>
