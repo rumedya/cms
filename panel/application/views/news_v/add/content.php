c430ae6da629acd792a56109609e7e19e2ea7091
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-25 20:26:37 +0300

    haber ekle sayfa tasarımı yapıldı ve haber türü seçimi yapıldı

--- panel/application/views/news_v/add/content.php
+++ panel/application/views/news_v/add/content.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12">
         <h4 class="m-b-lg">
-            Yeni Ürün Ekle
+            Yeni Haber Ekle
             <a href="#" type="button" class="btn pull-right btn-primary btn-xs btn-outline"><i class="fa fa-plus"></i> Yeni Ekle</a>
         </h4>
 
@@ -10,7 +10,7 @@
         <div class="widget">
 
             <div class="widget-body">
-                <form action="<?php echo base_url("product/save");?>" method="post">
+                <form action="<?php echo base_url("news/save");?>" method="post">
                     <div class="form-group">
                         <label >Başlık</label>
                         <input class="form-control" placeholder="Başlık" name="title">
@@ -24,8 +24,33 @@
                         <label>Açıklama</label>
                         <textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
                     </div>
+                    <div class="form-group">
+                        <label for="control-demo-6" class="">Haberin Türü</label>
+                        <div id="control-demo-6" class="">
+                            <select class="form-control news_type_select" name="news_type">
+                                <option value="image">Resim</option>
+                                <option value="video">Video</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group image_upload_container">
+                        <label>Görsel Seçiniz</label>
+                        <input type="file" name="img_url" class="form-control img_url">
+                    </div>
+
+
+                    <div class="form-group video_url_container">
+                        <label >Video URL</label>
+                        <input class="form-control video_url" placeholder="Video bağlantısını buraya yapıştırınız." name="video_url">
+                        <?php if(isset($form_error)){?>
+                            <small class="pull-right input-form-error">
+                                <?php echo form_error("video_url");?>
+                            </small>
+                        <?php }?>
+                    </div>
                     <button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
-                    <a href="<?php echo base_url("product");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                    <a href="<?php echo base_url("news");?>" class="btn btn-md btn-danger btn-outline">İptal</a>
                 </form>
             </div><!-- .widget-body -->
         </div><!-- .widget -->
