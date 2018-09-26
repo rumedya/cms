ea9ed410e3f646281d12621c98997f358d412025
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-26 19:18:32 +0300

    Haber insert işlemi gerçekleştirildi

--- panel/application/controllers/News.php
+++ panel/application/controllers/News.php
@@ -85,26 +85,80 @@ class News extends CI_CONTROLLER{
         //TRUE - FALSE
         $validate = $this->form_validation->run();
 
-        //Monitör Askısı
-        //monitor-askisi
-
-
         if($validate){
 
-            echo "kayıt işlemleri başlasın";
 
-            die();
 
-            $insert = $this->news_model->add(
-                array(
+
+            //upload süreci
+            if($news_type == "image"){
+
+                $file_name = convertToSEO(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME )) .".". pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION );
+
+                $config["allowed_types"]    = "png|jpg";
+                $config["upload_path"]      = "uploads/$this->viewFolder/";
+                $config["file_name"]        = $file_name;
+
+                $this->load->library("upload", $config);
+
+                $upload = $this->upload->do_upload("img_url");
+
+                if($upload){
+
+                    $uploaded_file = $this->upload->data("file_name");
+
+                    $data = array(
+                        "title"         =>  $this->input->post("title"),
+                        "description"   =>  $this->input->post("description"),
+                        "url"           =>  convertToSEO($this->input->post("title")),
+                        "news_type"     =>  $news_type,
+                        "img_url"       =>  $uploaded_file,
+                        "video_url"     =>  "#",
+                        "rank"          =>  "0",
+                        "isActive"      =>  1,
+                        "createdAt"     =>  date("Y-m-d H:i:s")
+                    );
+
+
+
+
+
+
+                }
+                else{
+
+                    $alert = array(
+                        "title"     =>  "İşlem Başarılı.",
+                        "text"      =>  "Görsel yüklenirken bir problem oluştu.",
+                        "type"      =>  "error"
+                    );
+                    $this->session->set_flashdata("alert", $alert);
+                    redirect(base_url("news/new_form"));
+
+                    die();
+
+
+                }
+
+
+            }
+            else if($news_type == "video"){
+
+                $data = array(
                     "title"         =>  $this->input->post("title"),
                     "description"   =>  $this->input->post("description"),
                     "url"           =>  convertToSEO($this->input->post("title")),
+                    "news_type"     =>  $news_type,
+                    "img_url"       =>  "#",
+                    "video_url"     =>  $this->input->post("video_url"),
                     "rank"          =>  "0",
                     "isActive"      =>  1,
                     "createdAt"     =>  date("Y-m-d H:i:s")
-                )
-            );
+                );
+            }
+
+            $insert = $this->news_model->add($data);
+
             //TODO ALERT SİSTEMİ EKLENECEK
             if($insert){
                 $alert = array(
@@ -121,11 +175,10 @@ class News extends CI_CONTROLLER{
             }
             //İşlemin sonucunu Session'a yazma işlemi...
             $this->session->set_flashdata("alert", $alert);
-            redirect(base_url("product"));
+            redirect(base_url("news"));
 
         }else{
             $viewData                   = new stdClass();
-
             /** View'e gönderilecek değişkenlerin set edilmesi */
             $viewData->viewFolder       = $this->viewFolder;
             $viewData->subViewFolder    = "add";
