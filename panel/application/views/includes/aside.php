f7ff95fa707ca1bfab81b87a552c16c94cf24537
rumedya <43114407+rumedya@users.noreply.github.com>
2018-09-27 20:01:24 +0300

    markalar modülü oluşturuldu listeleme sayfası yapıldı

--- panel/application/views/includes/aside.php
+++ panel/application/views/includes/aside.php
@@ -108,7 +108,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="javascript:void(0)">
+                    <a href="<?php echo base_url("brands");?>">
                         <i class="menu-icon zmdi zmdi-puzzle-piece zmdi-hc-lg"></i>
                         <span class="menu-text">Markalar</span>
                     </a>
